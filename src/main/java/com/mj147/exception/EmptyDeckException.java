8995ea066da101438442a0f8a0ba5ef12376c45a
mj147 <jaczewski147@gmail.com>
2020-04-26 11:01:28 +0200

    add data access layer

---
+++ src/main/java/com/mj147/exception/EmptyDeckException.java
@@ -0,0 +1,8 @@
+package com.mj147.exception;
+
+public class EmptyDeckException extends Exception {
+
+    public EmptyDeckException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/mj147/repository/cards/CardRepository.java
@@ -0,0 +1,14 @@
+package com.mj147.repository.cards;
+
+import com.mj147.domain.cards.Card;
+import com.mj147.exception.EntityDoesNotExistException;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CardRepository extends CrudRepository<Card, Long> {
+
+    default Card getById(Long cardId) {
+        return findById(cardId).orElseThrow(
+                () -> new EntityDoesNotExistException("Card id: " + cardId + " not found"));
+    }
+
+}
