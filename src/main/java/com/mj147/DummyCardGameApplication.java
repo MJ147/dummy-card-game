7adcb1bc8519bd32386b13a7bbe78dc03b5884c5
Marek Jaczewski <jaczewski147@gmail.com>
2020-04-29 20:04:08 +0200

    add configuration and running application class

---
+++ src/main/java/com/mj147/DummyCardGameApplication.java
@@ -0,0 +1,13 @@
+package com.mj147;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyCardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyCardGameApplication.class, args);
+	}
+
+}
